60632aeb93126af57f2bde4e5f8717c8444d0fae
M N Tushar <mntushar25@gmail.com>
2022-04-04 08:58:15 +0600

    add static file

--- AspDotNetCoreAdminLte/Startup.cs
+++ AspDotNetCoreAdminLte/Startup.cs
@@ -51,6 +51,10 @@ namespace AspDotNetCoreAdminLte
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                    name: "Admin",
+                    pattern: "{controller=Admin}/{action=AdminIndex}/{id?}");
             });
         }
     }
